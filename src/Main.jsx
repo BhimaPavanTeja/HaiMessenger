007e15b0e2842f24659d6adcbe1a3bdaf26ee934
pavan <124360829+BhimaPavanTeja@users.noreply.github.com>
2024-07-17 10:12:35 +0530

    Update Main.jsx

--- src/Main.jsx
+++ src/Main.jsx
@@ -8,7 +8,7 @@ const Main = ({user, Logout}) => {
     <h2>Welcome to Hai Messenger App by React Developers!</h2> 
         <img className='m-auto' src={user.photoURL} alt={user.displayName} />
         <h3>Username: {user.displayName}</h3>
-        <i><p>Mail: {user.email}</p></i>
+        <i><p>ur Mail: {user.email}</p></i>
         <button style={{ backgroundColor: 'red', color: 'white', padding: '7px', border: 'none' }} onClick={Logout}>Logout</button>
     </>
   )
