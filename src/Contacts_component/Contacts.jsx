dbd43f85528d99928fa9f8a906082c2245e5ff12
pavan <124360829+BhimaPavanTeja@users.noreply.github.com>
2024-07-28 14:01:03 +0530

    update contact scroll

--- src/Contacts_component/Contacts.jsx
+++ src/Contacts_component/Contacts.jsx
@@ -10,11 +10,11 @@ const data = [
   },
   {
     profilepic: k2,
-    username: 'pavan',
+    username: 'Pavan Teja',
   },
   {
     profilepic: k3,
-    username: 'karteek',
+    username: 'karthik',
   },
   {
     profilepic: k1,
@@ -22,11 +22,35 @@ const data = [
   },
   {
     profilepic: k2,
-    username: 'pavan',
+    username: 'Pavan Teja',
   },
   {
     profilepic: k3,
-    username: 'karteek',
+    username: 'karthik',
+  },
+  {
+    profilepic: k1,
+    username: 'Lakshmi kiran kalluru',
+  },
+  {
+    profilepic: k2,
+    username: 'Pavan Teja',
+  },
+  {
+    profilepic: k3,
+    username: 'karthik',
+  },
+  {
+    profilepic: k1,
+    username: 'Lakshmi kiran kalluru',
+  },
+  {
+    profilepic: k2,
+    username: 'Pavan Teja',
+  },
+  {
+    profilepic: k3,
+    username: 'karthik',
   },
 ];
 
@@ -37,24 +61,19 @@ const Contacts = ({ toggle, handleClick }) => {
         <div
           key={index}
           className={`flex items-center mb-2 transition-all duration-500 ease-in-out ${
-            toggle ? 'mx-4' : ''
+            toggle ? 'mx-4 justify-center' : 'm-3'
           } hover:bg-gray-600 rounded-xl cursor-pointer`}
           onClick={() => handleClick(contact)}
         >
           <img
             src={contact.profilepic}
             alt="Avatar"
-            className={`rounded-full min-w-12 h-12 m-2 cursor-pointer transition-all duration-500 ease-in-out ${
+            className={`object-cover rounded-full min-w-12 h-12 m-2 cursor-pointer transition-all duration-500 ease-in-out ${
               toggle ? 'mx-6 w-12 h-12' : ''
             }`}
           />
           {!toggle && (
-            <h1 className="text-white mt-3 font-medium whitespace-pre transition-opacity duration-500 ease-in-out opacity-100">
-              {contact.username}
-            </h1>
-          )}
-          {toggle && (
-            <h1 className="text-white mt-3 font-medium whitespace-pre transition-opacity duration-500 ease-in-out opacity-0">
+            <h1 className="text-white ml-1 font-medium whitespace-pre transition-opacity duration-500 ease-in-out opacity-100">
               {contact.username}
             </h1>
           )}
--- src/sidebar-component/ChatBox.jsx
+++ src/sidebar-component/ChatBox.jsx
@@ -1,17 +1,16 @@
 import React, { useState } from 'react';
+import '../Contacts_component/contact.css';
 import backIcon from './back-icon.png';
 import SearchBar from '../search-component/SearchBar';
 import Contacts from '../Contacts_component/Contacts';
 
-const ChatBox = ({ username , Logout }) => {
+const ChatBox = ({ username, Logout }) => {
   const [toggle, setToggle] = useState(false);
   const [showProfile, setShowProfile] = useState(false);
-
 
   const handleClick = () => {
     setToggle(!toggle);
   };
- 
 
   const handleProfileClick = () => {
     setShowProfile(!showProfile);
@@ -20,58 +19,66 @@ const ChatBox = ({ username , Logout }) => {
   return (
     <div className="relative h-screen">
       {/* Sidebar */}
-      <div
-        className={`absolute left-0 top-0 bottom-0 bg-gray-700 shadow-xl m-3 rounded-lg justify-between transition-width duration-500 ${
-          toggle ? 'w-1/12' : 'w-3/12'
-        }`}
-        style={{ animation: 'slide-in 0.5s ease-in-out' }}
-      >
-        <div className='flex relative'>
-          <img
-            src={username.photoURL}
-            alt="Avatar"
-            className={`rounded-full w-8 h-8 m-2 cursor-pointer`}
-            onClick={handleProfileClick}
-          />
-          {!toggle && (
-            <h1 className="text-white mt-3 font-medium whitespace-pre">{username.displayName}</h1>
-          )}
-          {showProfile && (
-            <div className="absolute top-12 left-2 w-60 bg-white shadow-lg rounded-lg p-4 z-10">
-              <h2 className='text-lg font-bold mb-3'>Profile</h2>
-              <img className='m-auto rounded' src={username.photoURL} alt={username.displayName} />
-              <h3 className='font-semibold mt-2 box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white w-fit px-2 m-auto rounded'>{username.displayName}</h3>
-              <p className='font-medium text-base italic'>{username.email}</p>
+      {showProfile && (
+            <div className="absolute top-16 left-5 w-60 bg-white shadow-lg rounded-lg p-4 z-10">
+              <h2 className="text-lg font-bold mb-3">Profile</h2>
+              <img
+                className="m-auto rounded"
+                src={username.photoURL}
+                alt={username.displayName}
+              />
+              <h3 className="font-semibold mt-2 box-decoration-clone bg-gradient-to-r from-indigo-600 to-pink-500 text-white w-fit px-2 m-auto rounded">
+                {username.displayName}
+              </h3>
+              <p className="font-medium text-base italic">{username.email}</p>
               <button
-                className='font-light mt-4 cursor-pointer bg-red-600 text-white px-2 py-1 rounded-lg'
+                className="font-light mt-4 cursor-pointer bg-red-600 text-white px-2 py-1 rounded-lg"
                 onClick={Logout}
               >
                 Logout
               </button>
             </div>
           )}
+      <div
+        className={`overflow-hidden absolute left-0 top-0 bottom-0 bg-gray-700 shadow-xl m-3 rounded-lg justify-between transition-width duration-500 ${
+          toggle ? 'w-1/12' : 'w-3/12'
+        }`}
+        style={{ animation: 'slide-in 0.5s ease-in-out' }}
+      >
+        <div className="flex relative">
           <img
-          src={backIcon}
-          alt="Back"
-          className={`cursor-pointer transition-transform duration-500 w-5 h-5 absolute right-0 ${toggle ? 'm-2' : 'mt-2'}`}
-          style={{ transform: toggle ? 'rotate(-90deg)' : 'rotate(0deg)' }}
-          onClick={handleClick}
-        />
+            src={username.photoURL}
+            alt="Avatar"
+            className="rounded-full w-8 h-8 m-2 cursor-pointer"
+            onClick={handleProfileClick}
+          />
+          {!toggle && (
+            <h1 className="text-white mt-3 font-medium whitespace-pre">
+              {username.displayName}
+            </h1>
+          )}
+          <img
+            src={backIcon}
+            alt="Back"
+            className={`cursor-pointer transition-transform duration-500 w-5 h-5 absolute right-0 ${
+              toggle ? 'm-2' : 'mt-2'
+            }`}
+            style={{ transform: toggle ? 'rotate(-90deg)' : 'rotate(0deg)' }}
+            onClick={handleClick}
+          />
         </div>
         <SearchBar toggle={toggle} />
         {/* contacts */}
-        <Contacts toggle={toggle} handleClick={handleClick} />
-
+        <div className="overflow-y-auto h-[calc(100%-80px)] hide-scrollbar">
+          <Contacts toggle={toggle} handleClick={handleClick} />
+        </div>
       </div>
       {/* ChatBox */}
       <div
         className={`absolute right-0 top-0 bottom-0 bg-emerald-50 shadow-xl m-3 rounded-lg transition-width duration-500 ${
-          toggle ? 'w-[89%]' : 'w-[72%]'
+          toggle ? 'w-[89.5%]' : 'w-[72.5%]'
         }`}
-      >
-        
-
-      </div>
+      ></div>
     </div>
   );
 };
